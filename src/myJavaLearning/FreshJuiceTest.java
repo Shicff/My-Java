6a55d95e617c2b930946b22a1205de21a1149d71
shchfeng@163.com <shchfeng@163.com>
2017-10-20 22:29:58 +0800

    add an anue of fruitsize

---
+++ src/myJavaLearning/FreshJuiceTest.java
@@ -0,0 +1,16 @@
+package myJavaLearning;
+
+
+class FreshJuice{
+	enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+	FreshJuiceSize size;
+}
+
+public class FreshJuiceTest {
+
+	public static void main(String args[]){
+		FreshJuice juice=new FreshJuice();
+		juice.size=FreshJuice.FreshJuiceSize.MEDIUM;
+		System.out.println(juice.size);
+	}
+}
